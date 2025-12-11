1486c888ab2e9ea244ec3457fc16d760103be3b3
Nene Nnoruka <237324102+clarioncbbii@users.noreply.github.com>
2025-12-11 16:06:27 +0000

    feat:client deployed

--- server/server.js
+++ server/server.js
@@ -1,12 +1,36 @@
 //TODO: Set up a server
 
-//imports
+//imports --> express + cors packages
+//use cors to allow our client to send data (cross-origin)
+//we also have to import db from dbConnection to use it later
+import express from "express";
+import cors from "cors";
+import { db } from "./dbConnection.js";
 
-//configs
+//remember to store express in a variable --> app
+const app = express();
 
-//port
+//config cors and express
+app.use(cors());
+app.use(express.json());
 
-//root route
+//PORT setup
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.info(`Server is live in port ${PORT}!`);
+});
+
+//TODO: READ a welcome message
+//ROOT route
+app.get("/", (req, res) => {
+  //res.json({message:"Welcome to the server. GET comfy!"}) --> only allows us to send JSON format
+  // you can  use send method to send non-JSON, such as HTML instead, in string format:
+  res.send("<h1>WELCOME TO THE SERVER :)</h1> <h2>GET COMFY BELOVED!</h2>");
+});
+
+app.listen(8080, function () {
+  console.log("App running on port 8080");
+});
 
 // ==============================
 
