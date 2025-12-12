f7b9d893ff927fcbe16cc0ace8e3e6257e4b86cc
Nene Nnoruka <237324102+clarioncbbii@users.noreply.github.com>
2025-12-12 17:37:55 +0000

    "chore: routine file push"

--- client/src/app.js
+++ client/src/app.js
@@ -55,3 +55,65 @@ function handleSubmit(submitEvent) {
 
 //Then, render the data using DOM elements --> one per piece of data
 //e.g. 4 columns of data requires 4 DOM elements
+
+// // ---------- from week 3 assignment ---------- //
+// //TODO: Create DOM elements for the shop upgrades
+// //- create the element
+
+// //- assign the value to its property (textContent)
+// //- append it to the DOM (appendChild)
+
+// //- after you complete this task, you should see the upgrades in your shop container!
+
+// // ** Create the function to create each upgrade in the shop **
+// // I am selecting the shop container and storing it in a variable
+// async function createUpgrades() {
+//   const itemData = await getData();
+//   const shopContainer = document.getElementById("shop-container");
+
+//   //for each item (upgrade) in the shop, I need the function to create a div + repeat a number of actions (loop):
+
+//   // I need it to create elements for each of its properties
+//   for (let i = 0; i < itemData.length; i++) {
+//     const shopItems = document.createElement("div");
+//     const upgradeName = document.createElement("h3");
+//     const upgradeCost = document.createElement("p");
+//     const upgradeIncrease = document.createElement("p");
+//     const buyButton = document.createElement("button");
+//     const itemImage = document.createElement("img");
+
+//     // I need it to update the textContent using the API data
+//     upgradeName.textContent = itemData[i].name;
+//     upgradeCost.textContent = `Cost: ${itemData[i].cost}`;
+//     upgradeIncrease.textContent = `CPS Increase: ${itemData[i].increase}`;
+//     buyButton.textContent = `BUY ME`;
+//     itemImage.src = images[i];
+
+//     // I need it to assign a class name
+//     upgradeName.className = "upgrade-name";
+//     upgradeCost.className = "upgrade-cost";
+//     upgradeIncrease.className = "upgrade-increase";
+//     shopItems.className = "shop-item";
+//     buyButton.className = "buy-button";
+//     itemImage.className = "item-image";
+
+//     // I need it to append the name, cost and increase to the div in question
+//     shopItems.appendChild(upgradeName);
+//     shopItems.appendChild(upgradeCost);
+//     shopItems.appendChild(upgradeIncrease);
+//     shopItems.appendChild(buyButton);
+//     shopItems.appendChild(itemImage);
+
+//     // I need to append each shop item container to the shop container for easier styling
+//     shopContainer.appendChild(shopItems);
+
+//     console.log(itemData[i]);
+
+//     //BECAUSE I WANT THIS TO HAPPEN ON CLICK OF BUTTON, I NEED AN EVENT LISTENER
+//     buyButton.addEventListener("click", function () {
+//       buyMe(itemData[i]);
+//     });
+//   }
+// }
+// //then I call the function:
+// createUpgrades();
